316ff5fa4bad2c33771601d7071944fa9cb0201e
Zemin <zemin.xu@yahoo.com>
2020-02-16 16:15:19 +0100

    environment

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject prefab;
+    public NavMeshAgent agent;
+
+    public Transform startP;
+    public Transform endP;
+
+
+    private void Start() {
+
+        GameObject go = Instantiate(prefab, startP);
+        agent = go.GetComponent<NavMeshAgent>();
+
+        agent.SetDestination(endP.position);
+        
+    }
+
+}
